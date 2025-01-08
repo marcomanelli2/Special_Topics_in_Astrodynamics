752690675faadbc794b927276752b1ffb3c687ca
marcomanelli2 <128060372+marcomanelli2@users.noreply.github.com>
2025-01-08 23:28:15 +0100

    resit assignment code commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Special Topics in Astrodynamics
+
+## Initialize the virtual environment
+
+```commandline
+python3 -m venv venv
+python3 -m pip install --upgrade pip
+source venv/bin/activate
+pip install -r requirements.txt
+```
--- functions.py
+++ functions.py
@@ -4,18 +4,22 @@ import numpy as np
 import numpy.linalg as LA
 import scipy
 
-M_SUN = 1.9891e+30
-M_EARTH = 6.0477e+24
+# Assignment constants
+M_SUN = 1.9891e+30  # kg
+M_EARTH = 6.0477e+24  # kg
+AU = 149597870.7e+3  # m
+R_SUN = 695700e+3  # m
+R_EARTH = 6378e+3  # m
+
 MU = M_EARTH / (M_SUN + M_EARTH)
-R_SUN = 695700000
-R_EARTH = 6378000
-AU = 149597870700
-
-MU_JPL = 3.0542E-6
+MU_JPL = 3.05420e-6
+X0_JPL = np.array(
+    [9.91416770988543e-01, 3.90169410418171e-23,  1.10992377703143e-02,
+     2.44188247527455e-15, 1.53784488470937e-02, -1.03706638670444e-14])
 L1_JPL = np.array([0.98997092, 0.00000000, 0.00000000])
-T_JPL = 2.7418266724357050E+0
+T_JPL = 2.73985394771395
 
-DIR = os.curdir
+DIR = os.getcwd()
 
 
 def equilibrium_eqn(x):
@@ -61,52 +65,54 @@ def variational_eqn(t, x, mu, beta):
     cos_alpha = np.dot(r1_dir, n)
     a_sail = beta * (1 - mu) / r1 ** 2 * cos_alpha ** 2
 
-    x_dot = np.zeros(42,)
+    x_dot = np.zeros(42, )
 
     # state derivative
     x_dot[0] = x[3]
     x_dot[1] = x[4]
     x_dot[2] = x[5]
-    x_dot[3] = 2 * x[4] + x[0] - (1 - mu) * (mu + x[0]) / r1**3 + mu * (1 - mu - x[0]) / r2**3 + a_sail
-    x_dot[4] = -2 * x[3] + x[1] * (1 - (1 - mu) / r1**3 - mu / r2**3)
-    x_dot[5] = -((1 - mu) / r1**3 + mu / r2**3) * x[2]
+    x_dot[3] = 2 * x[4] + x[0] - (1 - mu) * (mu + x[0]) / r1 ** 3 + mu * (1 - mu - x[0]) / r2 ** 3 + a_sail
+    x_dot[4] = -2 * x[3] + x[1] * (1 - (1 - mu) / r1 ** 3 - mu / r2 ** 3)
+    x_dot[5] = -((1 - mu) / r1 ** 3 + mu / r2 ** 3) * x[2]
 
     # state transition matrix
     Phi = x[6:].reshape(6, 6)
 
     # solar sail acceleration derivatives
-    a_xx = -2 * beta * (1 - mu) * (mu + x[0]) * (2 * (mu + x[0])**2 - r1**2) / r1**6
-    a_xy = -4 * beta * (1 - mu) * (x[0] + mu)**2 / r1**6 * x[1]
-    a_xz = -4 * beta * (1 - mu) * (x[0] + mu)**2 / r1**6 * x[2]
+    a_xx = -2 * beta * (1 - mu) * (mu + x[0]) * (2 * (mu + x[0]) ** 2 - r1 ** 2) / r1 ** 6
+    a_xy = -4 * beta * (1 - mu) * (x[0] + mu) ** 2 / r1 ** 6 * x[1]
+    a_xz = -4 * beta * (1 - mu) * (x[0] + mu) ** 2 / r1 ** 6 * x[2]
 
     # hessian matrix of the potential
-    U_xx = 1 + (1 - mu) / r1**5 * (2 * (x[0] + mu)**2 - x[1]**2 - x[2]**2) + mu / r2**5 * (2 * (x[0] - (1 - mu))**2
-                                                                                           - x[1]**2 - x[2]**2)
-    U_yy = 1 - (1 - mu) / r1**5 * ((x[0] + mu)**2 - 2 * x[1]**2 + x[2]**2) - mu / r2**5 * ((x[0] - (1 - mu))**2
-                                                                                           - 2 * x[1]**2 + x[2]**2)
-    U_zz = -((1 - mu) * ((x[0] + mu)**2 + x[1]**2 - 2 * x[2]**2) / r1 ** 5 + mu * ((1 - mu - x[0])**2 + x[1]**2
-                                                                                   - 2 * x[2]**2) / r2**5)
-    U_xy = 3 * x[1] * (1 - mu) * (x[0] + mu) / r1**5 + 3 * mu * x[1] * (x[0] - (1 - mu)) / r2**5
+    U_xx = 1 + (1 - mu) / r1 ** 5 * (2 * (x[0] + mu) ** 2 - x[1] ** 2 - x[2] ** 2) + mu / r2 ** 5 * (
+                2 * (x[0] - (1 - mu)) ** 2
+                - x[1] ** 2 - x[2] ** 2)
+    U_yy = 1 - (1 - mu) / r1 ** 5 * ((x[0] + mu) ** 2 - 2 * x[1] ** 2 + x[2] ** 2) - mu / r2 ** 5 * (
+                (x[0] - (1 - mu)) ** 2
+                - 2 * x[1] ** 2 + x[2] ** 2)
+    U_zz = -((1 - mu) * ((x[0] + mu) ** 2 + x[1] ** 2 - 2 * x[2] ** 2) / r1 ** 5 + mu * (
+                (1 - mu - x[0]) ** 2 + x[1] ** 2
+                - 2 * x[2] ** 2) / r2 ** 5)
+    U_xy = 3 * x[1] * (1 - mu) * (x[0] + mu) / r1 ** 5 + 3 * mu * x[1] * (x[0] - (1 - mu)) / r2 ** 5
 
-    U_xz = 3 * (1 - mu) * x[2] * (x[0] + mu) / r1**5 + 3 * mu * x[2] * (x[0] - (1 - mu)) / r2**5
+    U_xz = 3 * (1 - mu) * x[2] * (x[0] + mu) / r1 ** 5 + 3 * mu * x[2] * (x[0] - (1 - mu)) / r2 ** 5
 
-    U_yz = 3 * (1 - mu) * x[1] * x[2] / r1**5 + 3 * mu * x[1] * x[2] / r2**5
+    U_yz = 3 * (1 - mu) * x[1] * x[2] / r1 ** 5 + 3 * mu * x[1] * x[2] / r2 ** 5
 
     A = np.array([[0, 0, 0, 1, 0, 0],
-                 [0, 0, 0, 0, 1, 0],
-                 [0, 0, 0, 0, 0, 1],
-                 [U_xx + a_xx, U_xy + a_xy, U_xz + a_xz, 0, 2, 0],
-                 [U_xy, U_yy, U_yz, -2, 0, 0],
-                 [U_xz, U_yz, U_zz, 0, 0, 0]])
+                  [0, 0, 0, 0, 1, 0],
+                  [0, 0, 0, 0, 0, 1],
+                  [U_xx + a_xx, U_xy + a_xy, U_xz + a_xz, 0, 2, 0],
+                  [U_xy, U_yy, U_yz, -2, 0, 0],
+                  [U_xz, U_yz, U_zz, 0, 0, 0]])
 
     # state transition matrix derivative
-    x_dot[6:] = (A @ Phi).reshape(36,)
+    x_dot[6:] = (A @ Phi).reshape(36, )
 
     return x_dot
 
 
 def diff_correction(x_0, mu, beta):
-
     delta = np.array([0, 0])
     err = 1
     it = 0
@@ -118,7 +124,7 @@ def diff_correction(x_0, mu, beta):
         event = lambda t, x: x[1]
         event.terminal = True
 
-        sol = scipy.integrate.solve_ivp(lambda t, x: variational_eqn(t, x, mu, beta), [0, 2*T_JPL], x_0,
+        sol = scipy.integrate.solve_ivp(lambda t, x: variational_eqn(t, x, mu, beta), [0, 2 * T_JPL], x_0,
                                         rtol=1e-12, atol=1e-12, events=event)
 
         period = 2 * sol.t_events[0][0]
@@ -128,19 +134,20 @@ def diff_correction(x_0, mu, beta):
         # acceleration
         r1 = np.sqrt((mu + state_half[0]) ** 2 + state_half[1] ** 2 + state_half[2] ** 2)
         r2 = np.sqrt((1 - mu - state_half[0]) ** 2 + state_half[1] ** 2 + state_half[2] ** 2)
-        acc_x_half = 2 * state_half[4] + state_half[0] - (1 - mu) * (mu + state_half[0]) / r1 ** 3 + mu * (1 - mu - state_half[0]) / r2 ** 3
+        acc_x_half = 2 * state_half[4] + state_half[0] - (1 - mu) * (mu + state_half[0]) / r1 ** 3 + mu * (
+                    1 - mu - state_half[0]) / r2 ** 3
         acc_z_half = -((1 - mu) / r1 ** 3 + mu / r2 ** 3) * state_half[2]
         vel_y_half = state_half[4]
 
         # change x0 and vy0
-        M = np.array([[phi_half[3, 0], phi_half[3, 4]], [phi_half[5, 0], phi_half[5, 4]]]) - 1/vel_y_half * (
+        M = np.array([[phi_half[3, 0], phi_half[3, 4]], [phi_half[5, 0], phi_half[5, 4]]]) - 1 / vel_y_half * (
             np.array([[acc_x_half * phi_half[1, 0], acc_x_half * phi_half[1, 4]],
                       [acc_z_half * phi_half[1, 0], acc_z_half * phi_half[1, 4]]]))
         b = -np.array([state_half[3], state_half[5]])
         delta = np.linalg.solve(M, b)
 
         # error computation
-        err = np.sqrt(state_half[3]**2 + state_half[5]**2)
+        err = np.sqrt(state_half[3] ** 2 + state_half[5] ** 2)
 
         it += 1
         print(it, '\tError:\t', err)
